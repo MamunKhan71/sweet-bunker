f7d3dc7ff14e0233e7759e536713fb7317ad45cc
Md. Mamun <mkmamun031@gmail.com>
2025-07-09 17:05:23 +0600

    :bulb:UI fixes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import createNextIntlPlugin from 'next-intl/plugin';
+import type { NextConfig } from "next";
 
-const withNextIntl = createNextIntlPlugin();
+const nextConfig: NextConfig = {
+  /* config options here */
+};
 
-const nextConfig = {};
-
-export default withNextIntl(nextConfig);
+export default nextConfig;
